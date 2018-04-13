2e38b5d869a2a9da654a2241a488fe1d5ce71131
Jody Klymak <jklymak@gmail.com>
2018-04-13 11:20:45 -0700

    Version to try

--- code/SIZE.h
+++ code/SIZE.h
@@ -36,17 +36,17 @@ C     Nr  - No. points in Z for full process domain.
       INTEGER Ny
       INTEGER Nr
       PARAMETER (
-     &           sNx =  78,
-     &           sNy =  64,
+     &           sNx =  40,
+     &           sNy =  26,
      &           OLx =   4,
      &           OLy =   4,
      &           nSx =   1,
      &           nSy =   1,
-     &           nPx =   16,
+     &           nPx =   4,
      &           nPy =   2,
      &           Nx  = sNx*nSx*nPx,
      &           Ny  = sNy*nSy*nPy,
-     &           Nr  =  400)
+     &           Nr  =  340)
 
 C     MAX_OLX  - Set to the maximum overlap region size of any array
 C     MAX_OLY    that will be exchanged. Controls the sizing of exch
